698811b80303d8a0493951e1971d483aa5cef898
Matheus Francisco <matheusf_francisco@hotmail.com>
2023-01-17 22:12:05 -0300

    first commit

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Client;
+use Illuminate\Http\Request;
+
+class ClientController extends Controller
+{
+    public function store(Request $request)
+    {
+        Client::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'id_number' => $request->id_number
+        ]);
+
+        return redirect('/');
+    }
+}
